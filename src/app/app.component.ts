b2d4096d4742b88783086b5d9117ea7e0e8ef51e
kenisteward <keni.steward@gmail.com>
2020-03-09 00:10:06 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,57 @@ import { Component } from '@angular/core';
 export class AppComponent  {
   name = 'Angular';
 
+  schema = {
+    type: 'object',
+    definitions: {
+      states: {
+        type: "string",
+        oneOf: [{
+          const: 's1',
+          title: 'State 1'
+        },
+        {
+          const: 's2',
+          title: 'State 2'
+        },
+        {
+          const: 's3',
+          title: 'State 3'
+        }]
+        
+      },
+      commonAddress: {
+        type: "object",
+        properties: {
+          line1: {
+            "type": "string",
+          },
+          stateCode: {
+            $ref: "#/definitions/states"
+          }
+        }
+      }
+    },
+    properties: {
+      myAddress: {
+        $ref: '#/definitions/commonAddress'
+      },
+      myAddresses: {
+        type: 'array',
+        items: {
+          $ref: '#/definitions/commonAddress'
+        }
+      }
+    }
+  }
+
   ngOnInit( ) {
-    console.log(window.JsonRefs);
+    const options =         {resolveCirculars: true,
+        location: 'http://localhost:5000/ramp-schemas/forms/u4/u4.json',
+        loaderOptions: {
+            prepareRequest: (req, cb) => { req.withCredentials(true); cb(undefined, req); }
+        }};
+    JsonRefs.resolveRefs(this.schema).
+    then(e => { console.log(e.resolved)});
   }
 }
