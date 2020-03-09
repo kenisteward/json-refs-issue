e93227340c73a06441da6114bdd67baf8c43b6c0
kenisteward <keni.steward@gmail.com>
2020-03-09 01:03:50 -0500

    2 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,54 +9,20 @@ export class AppComponent  {
   name = 'Angular';
 
   schema = {
-    type: 'object',
-    definitions: {
-      states: {
-        type: "string",
-        oneOf: [{
-          const: 's1',
-          title: 'State 1'
-        },
-        {
-          const: 's2',
-          title: 'State 2'
-        },
-        {
-          const: 's3',
-          title: 'State 3'
-        }]
-        
-      },
-      commonAddress: {
-        type: "object",
-        properties: {
-          line1: {
-            "type": "string",
-          },
-          stateCode: {
-            $ref: "#/definitions/states"
-          }
-        }
+   "type": "object",
+   "properties": {
+      "sub": {
+        "$ref": "https://raw.githubusercontent.com/kenisteward/json-refs-issue/master/src/assets/defs.json"
       }
-    },
-    properties: {
-      myAddress: {
-        $ref: '#/definitions/commonAddress'
-      },
-      myAddresses: {
-        type: 'array',
-        items: {
-          $ref: '#/definitions/commonAddress'
-        }
-      }
-    }
-  }
+   }
+}
+
 
   res = {} ;
   
 
   ngOnInit( ) {
-    JsonRefs.resolveRefsAt('https://raw.githubusercontent.com/kenisteward/json-refs-issue/master/src/assets/base.json').
+    JsonRefs.resolveRefs(this.schema).
     then(e => { this.res = e;});
   }
 }
